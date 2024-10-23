77f093f078d7e722c49052e0b9f85b03f8ffbbd4
Aoutor <Aoutor.Zubchenko@gmail.com>
2024-10-23 19:21:45 -0400

    new

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-
-const lineNumbersContainer = document.getElementById('line-numbers');
-const numberOfLines = 30;
-for (let i = 1; i <= numberOfLines; i++) {
-    const lineNumber = document.createElement('p');
-    lineNumber.textContent = i;
-    lineNumbersContainer.appendChild(lineNumber);
-}
